92d814ccfaf562d18099f5dd714e40cc48ad8bdd
kes76963 <kes76963@gmail.com>
2021-06-29 13:10:13 +0900

    이미지에서 얼굴 가져오기

--- exam16_Beauty_GAN.py
+++ exam16_Beauty_GAN.py
@@ -17,6 +17,8 @@ def align_faces(img, detector, sp) : #이미지 정렬 함수
 detector = dlib.get_frontal_face_detector() #얼굴만 찾아줌
 sp = dlib.shape_predictor('./models/shape_predictor_5_face_landmarks.dat')
 
+#테스트 이미지
+"""
 test_img = dlib.load_rgb_image('./imgs/02.jpg')
 test_faces = align_faces(test_img, detector, sp)
 fig, axes = plt.subplots(1, len(test_faces)+1, figsize=(20,16))
@@ -24,6 +26,33 @@ axes[0].imshow(test_img)
 for i, face in enumerate(test_faces):
     axes[i+1].imshow(face)
 
+plt.show()
+"""
+
+#모델 load
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.import_meta_graph('./models/model.meta')
+saver.restore(sess, tf.train.latest_checkpoint('./models'))
+graph = tf.get_default_graph()
+X = graph.get_tensor_by_name('X:0')
+Y = graph.get_tensor_by_name('Y:0')
+Xs = graph.get_tensor_by_name('generator/xs:0')
+
+def preprocess(img):
+    return(img/255. - 0.5) * 2
+def deproess(img):
+    return (img + 1 ) / 2
+
+img1 = dlib.load_rgb_image('./imgs/12.jpg')
+img1_faces = align_faces(img1, detector, sp)
+
+img2 = dlib.load_rgb_image('./imgs/makeup/vFG56.png')
+img2_faces = align_faces(img2, detector, sp)
+
+fig, axes = plt.subplots(1,2,figsize=(16,10))
+axes[0].imshow(img1_faces[0])
+axes[1].imshow(img2_faces[0])
 plt.show()
 
 
