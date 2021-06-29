5f0221f990d3e81b3660c86fb8aacc4177256ff3
kes76963 <kes76963@gmail.com>
2021-06-29 12:57:44 +0900

    faces detection

--- exam16_Beauty_GAN.py
+++ exam16_Beauty_GAN.py
@@ -5,8 +5,30 @@ import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior() #뷰티gan이 tf v1에서 만들어진 거라서
 import numpy as np
 
+def align_faces(img, detector, sp) : #이미지 정렬 함수
+    dets = detector(img, 1)
+    objs = dlib.full_object_detections()
+    for detection in dets :
+        s = sp(img, detection)
+        objs.append(s)
+    faces = dlib.get_face_chips(img, objs, size=256, padding=0.35)
+    return faces
+
 detector = dlib.get_frontal_face_detector() #얼굴만 찾아줌
 sp = dlib.shape_predictor('./models/shape_predictor_5_face_landmarks.dat')
+
+test_img = dlib.load_rgb_image('./imgs/02.jpg')
+test_faces = align_faces(test_img, detector, sp)
+fig, axes = plt.subplots(1, len(test_faces)+1, figsize=(20,16))
+axes[0].imshow(test_img)
+for i, face in enumerate(test_faces):
+    axes[i+1].imshow(face)
+
+plt.show()
+
+
+"""
+얼굴 인식
 
 img = dlib.load_rgb_image('./imgs/12.jpg')
 plt.figure(figsize=(16,10))
@@ -35,23 +57,13 @@ for detection in dets :
         circle = patches.Circle((point.x, point.y),radius=3, edgecolor='r',facecolor='r')
         ax.add_patch(circle)
 ax.imshow(img_result)
-plt.show()
+#plt.show()
 
 faces = dlib.get_face_chips(img, objs, size=256, padding=0.3) #패딩이 얼굴 부분 확대 0이면 얼굴만 보여줌
-fig, axes = plt.subplots(1, len(faces)+1, figsize=(20.16))
+fig, axes = plt.subplots(1, len(faces)+1, figsize=(20,16))
 axes[0].imshow(img)
 
 for i, face in enumerate(faces) :
     axes[i+1].imshow(face)
 plt.show()
-
-
-"""
-def align_faces(img) : #이미지 정렬 함수
-    dets = detector(img, 1)
-    objs = dlib.full_object_detections()
-    for detection in dets :
-        s = sp(img, detection)
-        objs.append(s)
-    faces = dlib.get
 """
