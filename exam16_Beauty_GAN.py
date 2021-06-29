480a45b5b273d01f8ecd97090e9259a9a54b886d
kes76963 <kes76963@gmail.com>
2021-06-29 12:17:02 +0900

    faces detection

--- exam16_Beauty_GAN.py
+++ exam16_Beauty_GAN.py
@@ -13,3 +13,45 @@ plt.figure(figsize=(16,10))
 plt.imshow(img)
 plt.show()
 
+img_result = img.copy()
+dets = detector(img) #얼굴의 좌표를 만들어준다
+if len(dets) == 0 :
+    print('cannot find faces!')
+else :
+    fig, ax = plt.subplots(1, figsize=(16,10))
+    for det in dets :
+        x, y, w, h = det.left(), det.top(), det.width(), det.height() #왼쪽, 위, 폭, 높이 좌표가 있음
+        rect = patches.Rectangle((x,y),w,h, linewidth=2, edgecolor='r', facecolor='none') # 네모 그렸을 때 선 두께, 색, 내부 채우기 색
+        ax.add_patch(rect)
+    ax.imshow(img_result)
+    plt.show()
+
+fig, ax = plt.subplots(1, figsize=(16,10))
+objs = dlib.full_object_detections()
+for detection in dets :
+    s = sp(img,detection) #학습된 모델
+    objs.append(s)
+    for point in s.parts() :
+        circle = patches.Circle((point.x, point.y),radius=3, edgecolor='r',facecolor='r')
+        ax.add_patch(circle)
+ax.imshow(img_result)
+plt.show()
+
+faces = dlib.get_face_chips(img, objs, size=256, padding=0.3) #패딩이 얼굴 부분 확대 0이면 얼굴만 보여줌
+fig, axes = plt.subplots(1, len(faces)+1, figsize=(20.16))
+axes[0].imshow(img)
+
+for i, face in enumerate(faces) :
+    axes[i+1].imshow(face)
+plt.show()
+
+
+"""
+def align_faces(img) : #이미지 정렬 함수
+    dets = detector(img, 1)
+    objs = dlib.full_object_detections()
+    for detection in dets :
+        s = sp(img, detection)
+        objs.append(s)
+    faces = dlib.get
+"""
