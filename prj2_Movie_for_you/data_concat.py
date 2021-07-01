3e22b6489c786b46321545164f7c65615f2c2427
kes76963 <kes76963@gmail.com>
2021-07-01 16:54:53 +0900

    영화 리뷰 모두 합치기

--- prj2_Movie_for_you/data_concat.py
+++ prj2_Movie_for_you/data_concat.py
@@ -1,30 +1,30 @@
+# 모듈 임포트
 import pandas as pd
 
-# #중복제거 / 원래는 크롤링 후에 했어야 했음
-# df_dup = pd.read_csv('./crawling/cleaned_review_2020.csv',index_col =0)
-#
+# # 중복제거
+# df_dup = pd.read_csv('./crawling/cleaned_review_2017.csv', index_col=0)
 # df_undup = df_dup.drop_duplicates()
-# print(df_undup.duplicated().sum())
-# df_undup.to_csv('./crawling/cleaned_review_2020.csv')
+# print(df_undup.duplicated().sum()) # 중복제거 확인
+#
+# df_undup.to_csv('./crawling/cleaned_review_2019.csv') # 중복이 0이면 저장
+# exit() # 프로그램 종료
 
-df = pd.read_csv('./crawling/one_sentence_reveiw_2017.csv', index_col=0)
-#df2 = pd.read_csv('./crawling/cleaned_review_2020 (1).csv', index_col=0)
-
+# 첫(기준) 데이터 로드
+df = pd.read_csv('./crawling/one_sentence_review_2017.csv', index_col=0)
 print(df.info())
-df_dup.drop_duplicates()
+
+df.drop_duplicates()
 df.dropna(inplace=True)
 print(df.info())
 
-df.columns['titles','cleaned_reviews']
+df_temp.columns = ['titles', 'cleaned_reviews']
 
-for i in range(18,22) :
-    df_temp = pd.read_csv(f'./crawling/one_sentence_reveiw_20{i}.csv')
-    df_dup.drop_duplicates()
-    df_temp.dropna(inplace=True)
-    df_temp.columns['titles', 'cleaned_reviews']
-    df = pd.concat([df, df_temp],ignore_index=True)
-
+for i in range(18, 21):
+    df_temp = pd.read_csv(f'./crawling/one_sentence_review_20{i}.csv', index_col=0) # 이어붙일 데이터들 로드
+    df_temp.dropna(inplace=True) # nan값 제거
+    df_temp.drop_duplicates() # 중복 제거
+    df_temp.columns = ['titles', 'cleaned_reviews'] # 컬럼명 변경
+    df_temp.to_csv(f'./crawling/one_sentence_review_20{i}.csv') # 저장
+    df = pd.concat([df, df_temp], ignore_index=True) # 이어붙이기
 print(df.info())
-df.to_csv('./crawling/movie_review_2017_2020')
-
-
+df.to_csv('./crawling/one_sentence_review_2017_2020.csv')
