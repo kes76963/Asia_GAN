3188bb638297a3d84d724b6536c6825bad113070
kes76963 <kes76963@gmail.com>
2021-07-01 11:34:59 +0900

    2019년 영화 리뷰 전처리

--- prj2_Movie_for_you/crawling/preprocess.py
+++ prj2_Movie_for_you/crawling/preprocess.py
@@ -2,12 +2,12 @@ import pandas as pd
 from konlpy.tag import Okt
 import re
 
-df = pd.read_csv('/content/crawling/reviews_2019_part.csv', index_col=0)
+df = pd.read_csv('./reviews_2019.csv', index_col=0)
 print(df.head())
 
 okt = Okt()
 
-stopwords = pd.read_csv('/content/crawling/stopwords.csv', index_col= 0)
+stopwords = pd.read_csv('./stopwords.csv', index_col= 0)
 
 movie_stopwords = ['영화','배우','감독']
 stopwords_list = list(stopwords.stopword) + movie_stopwords
@@ -20,7 +20,7 @@ for sentence in df.reviews :
     print('.', end='')
   if count % 100 == 0 :
     print('') #줄바꿈
-  sentence = re.sub('[^가-힣 | ' ']','',sentence)
+  sentence = re.sub('[^가-힣 ]','',sentence)
   token = okt.pos(sentence, stem = True) #stem true로 하면 원형으로 바꿔줌
   df_token = pd.DataFrame(token, columns=['word','class'])
   df_cleaned_token = df_token[(df_token['class'] =='Noun') |
