4c7bcaa8631376e8e0bb8038638da1e1c1957936
kes76963 <kes76963@gmail.com>
2021-07-02 15:18:11 +0900

    영화 추천 시스템 시각화

--- prj2_Movie_for_you/word2vec_visualization.py
+++ prj2_Movie_for_you/word2vec_visualization.py
@@ -11,6 +11,47 @@ mpl.rcParams['axes.unicode_minus'] = False
 rc('font', family=font_name)
 
 embedding_model = Word2Vec.load('./models/word2VecModel_2016_2021.model')
-key_word = '친구'
+key_word = '서하'
 sim_word = embedding_model.wv.most_similar(key_word, topn=10)
 print(sim_word)
+
+vectors=[]
+labels = []
+for label, _ in sim_word :
+    labels.append(label)
+    print(label)
+    vectors.append(embedding_model.wv[label])
+df_vectors = pd.DataFrame(vectors)
+print(df_vectors.head()) #100차원 공간간
+
+tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500,
+                  random_state= 23)
+new_values = tsne_model.fit_transform(df_vectors)
+df_xy = pd.DataFrame({'words':labels, 'x':new_values[:,0], 'y':new_values[:,1]}) #2차원좌표
+print(df_xy.head())
+
+print(df_xy.shape)
+df_xy.loc[df_xy.shape[0]] = (key_word, 0, 0)
+plt.figure(figsize=(8,8))
+plt.scatter(0,0,s=15000, marker='*') #산점도 / s는 카머의 크기
+for i in range(len(df_xy.x)):
+    a = df_xy.loc[[i,10],:]
+    plt.plot(a.x, a.y, '-D', linewidth=2)
+    plt.annotate(df_xy.words[i],xytext=(5,2),
+                 xy=(df_xy.x[i], df_xy.y[i]),
+                 textcoords='offset points', ha ='right',va ='bottom') #horizontal
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
